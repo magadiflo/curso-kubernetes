0c92685e6d614b900f4fdf9bf76d2cce4af166bc
Martín <magadiflo@gmail.com>
2023-02-02 16:54:58 -0500

    Implementando el controlador RestController y métodos handler GET

---
+++ dk-ms-usuarios/src/main/java/com/magadiflo/dk/ms/usuarios/app/resources/UsuarioResource.java
@@ -0,0 +1,39 @@
+package com.magadiflo.dk.ms.usuarios.app.resources;
+
+import com.magadiflo.dk.ms.usuarios.app.models.entity.Usuario;
+import com.magadiflo.dk.ms.usuarios.app.services.IUsuarioService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "/api/v1/usuarios")
+public class UsuarioResource {
+
+    private final IUsuarioService usuarioService;
+
+    public UsuarioResource(IUsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Usuario>> listarTodos() {
+        return ResponseEntity.ok(this.usuarioService.listar());
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Usuario> detalle(@PathVariable Long id) {
+        return this.usuarioService.porId(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Usuario> guardar(@RequestBody Usuario usuario) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(this.usuarioService.guardar(usuario));
+    }
+}
