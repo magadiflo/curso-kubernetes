8348b7e82ff5eb23190640a5cfac16e6c22984c4
Martín <magadiflo@gmail.com>
2023-02-02 16:36:12 -0500

    Implementando el componente Service

---
+++ dk-ms-usuarios/src/main/java/com/magadiflo/dk/ms/usuarios/app/services/IUsuarioService.java
@@ -0,0 +1,16 @@
+package com.magadiflo.dk.ms.usuarios.app.services;
+
+import com.magadiflo.dk.ms.usuarios.app.models.entity.Usuario;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IUsuarioService {
+    List<Usuario> listar();
+
+    Optional<Usuario> porId(Long id);
+
+    Usuario guardar(Usuario usuario);
+
+    void eliminar(Long id);
+}
---
+++ dk-ms-usuarios/src/main/java/com/magadiflo/dk/ms/usuarios/app/services/impl/UsuarioServiceImpl.java
@@ -0,0 +1,44 @@
+package com.magadiflo.dk.ms.usuarios.app.services.impl;
+
+import com.magadiflo.dk.ms.usuarios.app.models.entity.Usuario;
+import com.magadiflo.dk.ms.usuarios.app.repositories.IUsuarioRepository;
+import com.magadiflo.dk.ms.usuarios.app.services.IUsuarioService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UsuarioServiceImpl implements IUsuarioService {
+
+    private final IUsuarioRepository usuarioRepository;
+
+    public UsuarioServiceImpl(IUsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Usuario> listar() {
+        return (List<Usuario>) this.usuarioRepository.findAll();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<Usuario> porId(Long id) {
+        return this.usuarioRepository.findById(id);
+    }
+
+    @Override
+    @Transactional
+    public Usuario guardar(Usuario usuario) {
+        return this.usuarioRepository.save(usuario);
+    }
+
+    @Override
+    @Transactional
+    public void eliminar(Long id) {
+        this.usuarioRepository.deleteById(id);
+    }
+}
