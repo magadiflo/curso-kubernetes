44275af06af1ad870649f3c6f4dc8a03d302efad
Martín <magadiflo@gmail.com>
2023-02-03 10:09:38 -0500

    Escribiendo el controlador RestController para cursos

--- dk-ms-cursos/src/main/java/com/magadiflo/dk/ms/cursos/app/resources/CursoResource.java
+++ dk-ms-cursos/src/main/java/com/magadiflo/dk/ms/cursos/app/resources/CursoResource.java
@@ -2,6 +2,7 @@ package com.magadiflo.dk.ms.cursos.app.resources;
 
 import com.magadiflo.dk.ms.cursos.app.entity.Curso;
 import com.magadiflo.dk.ms.cursos.app.services.ICursoService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,4 +29,32 @@ public class CursoResource {
                 .map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
+
+    @PostMapping
+    public ResponseEntity<Curso> guardar(@RequestBody Curso curso) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(this.cursoService.guardar(curso));
+    }
+
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<Curso> editar(@PathVariable Long id, @RequestBody Curso curso) {
+        return this.cursoService.porId(id)
+                .map(cursoBD -> {
+                    cursoBD.setNombre(curso.getNombre());
+                    return ResponseEntity.status(HttpStatus.CREATED)
+                            .body(this.cursoService.guardar(cursoBD));
+                })
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<?> eliminar(@PathVariable Long id) {
+        return this.cursoService.porId(id)
+                .map(curso -> {
+                    this.cursoService.eliminar(curso.getId());
+                    return ResponseEntity.noContent().build();
+                })
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
 }
