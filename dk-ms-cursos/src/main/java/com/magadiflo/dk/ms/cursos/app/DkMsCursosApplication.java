2518dd3a8a71cbec494d4c0b0d1b9a26df652c1a
Martín <magadiflo@gmail.com>
2023-02-03 09:39:55 -0500

    Creando microservicios cursos y agregandolo al proyecto modular

---
+++ dk-ms-cursos/src/main/java/com/magadiflo/dk/ms/cursos/app/DkMsCursosApplication.java
@@ -0,0 +1,13 @@
+package com.magadiflo.dk.ms.cursos.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DkMsCursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DkMsCursosApplication.class, args);
+	}
+
+}
---
+++ dk-ms-cursos/src/test/java/com/magadiflo/dk/ms/cursos/app/DkMsCursosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magadiflo.dk.ms.cursos.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DkMsCursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
