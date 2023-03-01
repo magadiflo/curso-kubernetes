327d025671147b2eedc3b79f868f83a0995569ce
Martín <magadiflo@gmail.com>
2023-03-01 11:24:26 -0500

    Dockerizando microservicios cursos y configurando red o network

--- dk-ms-usuarios/src/main/java/com/magadiflo/dk/ms/usuarios/app/clients/ICursoClientFeign.java
+++ dk-ms-usuarios/src/main/java/com/magadiflo/dk/ms/usuarios/app/clients/ICursoClientFeign.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "dk-ms-cursos", url = "http://host.docker.internal:8002", path = "/api/v1/cursos")
+@FeignClient(name = "dk-ms-cursos", url = "http://dk-ms-cursos:8002", path = "/api/v1/cursos")
 public interface ICursoClientFeign {
     @DeleteMapping(path = "/eliminar-usuario/{usuarioId}")
     void eliminarCursoUsuarioPorId(@PathVariable Long usuarioId);
