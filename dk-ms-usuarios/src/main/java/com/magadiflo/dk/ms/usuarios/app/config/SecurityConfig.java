7dac8fdb791148761311e8d81ebcc0496a053cf7
Martín <magadiflo@gmail.com>
2023-04-06 12:08:07 -0500

    Configurando OAuth2ResourceServer

--- dk-ms-usuarios/src/main/java/com/magadiflo/dk/ms/usuarios/app/config/SecurityConfig.java
+++ dk-ms-usuarios/src/main/java/com/magadiflo/dk/ms/usuarios/app/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.magadiflo.dk.ms.usuarios.app.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,12 +13,19 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().authenticated()
+        http.authorizeRequests()
+                .antMatchers("/authorized").permitAll()
+                .antMatchers(HttpMethod.GET, "/", "/{id}").hasAnyAuthority("SCOPE_read", "SCOPE_write")
+                .antMatchers(HttpMethod.POST, "/").hasAuthority("SCOPE_write")
+                .antMatchers(HttpMethod.PUT, "/{id}").hasAuthority("SCOPE_write")
+                .antMatchers(HttpMethod.DELETE, "/{id}").hasAuthority("SCOPE_write")
+                .anyRequest().authenticated()
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .oauth2Login(oaut2Login -> oaut2Login.loginPage("/oauth2/authorization/dk-ms-usuarios-client"))
-                .oauth2Client(Customizer.withDefaults());
+                .oauth2Client(Customizer.withDefaults())
+                .oauth2ResourceServer().jwt();
         return http.build();
     }
 
