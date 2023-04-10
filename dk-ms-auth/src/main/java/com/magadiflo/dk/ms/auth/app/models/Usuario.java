51b44e09f69001b8c848fa88872fe81bfd48729d
Martín <magadiflo@gmail.com>
2023-04-10 13:07:52 -0500

    Implementando clase UserDetailsService para login personalizado con WebClient

---
+++ dk-ms-auth/src/main/java/com/magadiflo/dk/ms/auth/app/models/Usuario.java
@@ -0,0 +1,51 @@
+package com.magadiflo.dk.ms.auth.app.models;
+
+public class Usuario {
+    private Long id;
+    private String nombre;
+    private String email;
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Usuario{");
+        sb.append("id=").append(id);
+        sb.append(", nombre='").append(nombre).append('\'');
+        sb.append(", email='").append(email).append('\'');
+        sb.append(", password='").append(password).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
--- dk-ms-auth/src/main/java/com/magadiflo/dk/ms/auth/app/services/UsuarioService.java
+++ dk-ms-auth/src/main/java/com/magadiflo/dk/ms/auth/app/services/UsuarioService.java
@@ -1,23 +1,53 @@
 package com.magadiflo.dk.ms.auth.app.services;
 
+import com.magadiflo.dk.ms.auth.app.models.Usuario;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Collections;
+
 @Service
 public class UsuarioService implements UserDetailsService {
 
     private final WebClient webClient;
+
+    private static final Logger LOG = LoggerFactory.getLogger(UsuarioService.class);
 
     public UsuarioService(WebClient webClient) {
         this.webClient = webClient;
     }
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        try {
+            Usuario usuario = this.webClient.get()
+                    .uri("http://dk-ms-usuarios:8001/login", uriBuilder -> uriBuilder.queryParam("email", email).build())
+                    .accept(MediaType.APPLICATION_JSON)
+                    .retrieve()
+                    .bodyToMono(Usuario.class)
+                    .block();
+
+            LOG.info("Usuario login: {}", usuario.getEmail());
+            LOG.info("Usuario nombre: {}", usuario.getNombre());
+            LOG.info("Usuario password: {}", usuario.getPassword());
+
+            return new User(email, usuario.getPassword(),
+                    true, true, true, true,
+                    Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")));
+        } catch (RuntimeException e) {
+            String error = String.format("Error en el login, no existe el usuario en el sistema %s en el sistema", email);
+            LOG.error(error);
+            LOG.error(e.getMessage());
+            throw new UsernameNotFoundException(error);
+        }
     }
 
 }
